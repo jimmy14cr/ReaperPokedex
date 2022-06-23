51c34e3ba6cdce3fd43dc8692f9464cbce8e55c2
jimmy14cr <jimmybolanos14@gmail.com>
2022-06-23 17:38:17 -0600

    Deleting useless stuff

--- reaper-pokedex/src/App.js
+++ reaper-pokedex/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import Button from 'react-bootstrap/Button';
 import './App.css';
-import AjaxHooks from './Components/AjaxHooks';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './Components/Main';
 
